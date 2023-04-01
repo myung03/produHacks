06f1061b08ea685204159d4aaf042d45e436f1e6
nicholasfong <nfong01@students.cs.ubc.ca>
2023-04-01 13:49:58 -0700

    set up backend server with dependencies nodemon, mongoose, cors, and express, installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Home from './pages/Home';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      < Home/>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
