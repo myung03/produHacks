72ac9845ad895a7a0670e7536d67a98387baeb8d
Matthew <matthewyungisworking@gmail.com>
2023-04-01 16:42:25 -0700

    added animation function + constants

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Home from "./pages/Home";
 import { BottomBar, Hero } from './components/index';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import RegisterPage from "./pages/RegisterPage";
 import VideoPage from "./pages/VideoPage";
@@ -10,10 +11,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      < BottomBar></BottomBar>
+      <div className="app">
+        <Hero></Hero>
+      <BottomBar></BottomBar>
       <VideoPage />
-    </div>
+      </div>
   );
 }
 
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../constants/motion';
+import { motion, AnimatePresence } from 'framer-motion';
+import style from '../style';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <AnimatePresence>
+       <motion.div className="text-center" {...slideAnimation('left')}>
+        <h1 className={`text-[92px] ${style.heading2}`}>Testing</h1>
+       </motion.div>
+    </AnimatePresence>
   )
 }
 
