92887b0af93cd4763817b2e3c5f435de4eebd43f
Matthew <matthewyungisworking@gmail.com>
2023-04-02 04:39:13 -0700

    completed hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
       <h1 className={`${style.heading2} text-center text-gradient`}>
         Make Exercise Less Boring!
       </h1>
-      <p className="text-[#827979] text-center">Connect with friends like never before,
+      <p className="text-[#827979] text-center mt-[1rem]">Connect with friends like never before,
 all while performing tasks for healthy living! </p>
-<div className={`${style.flexCenter} `}>
-  <div className="dot-gradient rounded-full w-[20px] h-[20px]"/>
-  <div className="dot-gradient rounded-full w-[20px] h-[20px]"/>
-  <div className="dot-gradient rounded-full w-[20px] h-[20px]"/>
-      <img src={heroimg} className="w-[60%] h-[250px] mt-[2rem]"/>
+<div className={`flex justify-end align-center`}>
+  <div className="left-1 top-10 relative dot-gradient rounded-full w-[20px] h-[20px]"/>
+  <div className="right-12 top-40 relative dot-gradient rounded-full w-[20px] h-[20px]"/>
+  <div className="left-3 top-32 relative dot-gradient rounded-full w-[20px] h-[20px]"/>
+      <img src={heroimg} className="w-[60%] h-[250px] mt-[2rem] mr-[2rem]"/>
       </div>
     </div>
   )
