51b09468c4f64e0d864e34227346db235e20cb30
Matthew <matthewyungisworking@gmail.com>
2023-04-02 06:25:05 -0700

    completed login page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
 
           <div 
           className={`${toggle ? 'flex' : 'hidden'} p-6 dot-gradient absolute top-14 right-0 mx-4 my-2
-          min-w-[140px] rounded sidebar`}>
-              <ul className="text-white list-none flex flex-col gap-4 justify-end items-center flex-1">
+          min-w-[140px] rounded sidebar glassmorphism`}>
+              <ul className="text-black list-none flex flex-col gap-4 justify-end items-center flex-1">
             <li>Login</li>
             <li>Register</li>
           </ul>
