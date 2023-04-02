dc68abc82faab6319ad293af4b551a7ccdf2cc23
Matthew <matthewyungisworking@gmail.com>
2023-04-02 03:39:43 -0700

    added nav bar

---
+++ client/assets/index.js
@@ -0,0 +1,7 @@
+import heart from "./heart.png";
+import hamb from "./hamburger.png";
+import prompt from "./Saly-38.svg";
+import hero from "./Saly-40.svg";
+import close from "./close.png";
+
+export { heart, hamb, prompt, hero, close };
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState, React} from 'react';
+import style from '../style';
+import { hamb, close } from '../../assets/index';
 
 const Navbar = () => {
+    const [toggle, setToggle ] = useState(false)
   return (
-    <div>Navbar</div>
+
+    <div className={`flex justify-between align-middle ${style.paddingX}`}>
+        <img src={hamb} alt="logo"/>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={toggle ? close : hamb}
+            alt="menu"
+            className="w=[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle((prev) => !prev)}/>
+          </div>
+
+          <div 
+          className={`${toggle ? 'flex' : 'hidden'} p-6 dot-gradient absolute top-10 right-0 mx-4 my-2
+          min-w-[140px] rounded sidebar`}>
+              <ul className="text-white list-none flex flex-col gap-4 justify-end items-center flex-1">
+            <li>Login</li>
+            <li>Register</li>
+          </ul>
+          </div>
+    </div>
   )
 }
 
